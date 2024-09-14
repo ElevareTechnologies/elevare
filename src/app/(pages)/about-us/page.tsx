3be25f1e6797eca52f52cbe94899e088ac6024e8
Sirajudeen K.B <kbsirajudeen.cse@gmail.com>
2024-09-14 13:08:39 +0530

    changed background for card and moved it in separate component.

--- src/app/(pages)/about-us/page.tsx
+++ src/app/(pages)/about-us/page.tsx
@@ -6,13 +6,10 @@ import IMG1 from "./images/IMG1.png";
 import IMG2 from "./images/IMG2.png";
 import IMG3 from "./images/IMG3.png";
 import IMG4 from "./images/IMG4.png";
-import web from "./images/Group.png";
-import seo from "./images/startup.png";
-import ar from "./images/cloud-computing 1.png";
-import mobile from "./images/coding.png";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import SubscribeForm from "@/components/SubscribeForm";
+import { ServiceCard } from "../../../../public/ServiceCard";
 export default function Home() {
   return (
     <>
@@ -94,55 +91,7 @@ export default function Home() {
               </div>
             </div>
             <div className="w-full lg:w-1/2">
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-8 bg-white rounded-lg shadow-lg p-6 lg:p-10">
-                {/* Web Application */}
-                <div className="flex items-center space-x-4 p-4 bg-red-50 rounded-lg">
-                  <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
-                    <Image
-                      src={web}
-                      alt="Web Application"
-                      className="w-6 h-6"
-                    />
-                  </div>
-                  <h3 className="text-base lg:text-lg font-semibold text-purple-900">
-                    Web Application
-                  </h3>
-                </div>
-
-                {/* SEO */}
-                <div className="flex items-center space-x-4 p-4 bg-blue-50 rounded-lg">
-                  <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
-                    <Image src={seo} alt="SEO" className="w-6 h-6" />
-                  </div>
-                  <h3 className="text-base lg:text-lg font-semibold text-purple-900">
-                    SEO
-                  </h3>
-                </div>
-
-                {/* Mobile Applications */}
-                <div className="flex items-center space-x-4 p-4 bg-purple-50 rounded-lg">
-                  <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
-                    <Image
-                      src={mobile}
-                      alt="Mobile Applications"
-                      className="w-6 h-6"
-                    />
-                  </div>
-                  <h3 className="text-base lg:text-lg font-semibold text-purple-900">
-                    Mobile Applications
-                  </h3>
-                </div>
-
-                {/* AR/VR */}
-                <div className="flex items-center space-x-4 p-4 bg-pink-50 rounded-lg">
-                  <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
-                    <Image src={ar} alt="AR/VR" className="w-6 h-6" />
-                  </div>
-                  <h3 className="text-base lg:text-lg font-semibold text-purple-900">
-                    AR/VR
-                  </h3>
-                </div>
-              </div>
+              <ServiceCard />
             </div>
           </div>
         </div>
